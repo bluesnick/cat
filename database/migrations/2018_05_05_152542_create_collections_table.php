62d0a5acaf0348051456e382c174a606441f2572
ubuntu <ubuntu@localhost.localdomain>
2018-05-06 15:35:58 +0800

    init database

---
+++ database/migrations/2018_05_05_152542_create_collections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+			$table->unsignedInteger('uid');
+			$table->foreign('uid')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+}
